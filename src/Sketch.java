50a53bcd2429e220262526bc983cae534a218afc
denny7794 <denny7794@gmail.com>
2016-07-15 10:37:20 +0400

    bird image (J)

--- src/Sketch.java
+++ src/Sketch.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 /**
  * Created by Denis on 14.07.2016.
@@ -9,18 +10,21 @@ public class Sketch extends PApplet {
     float ypos = 50f;
     float vy = 0f;
     float gravity = 0.7f;
+    PImage bird;
 
     public void settings() { size(800, 600);}
 
     public void setup(){
         smooth();
         noStroke();
+        bird = loadImage("bird1.png");
     }
 
     public void draw(){
         background(0);
-        fill(255);
-        rect(xpos, ypos, 40, 40);
+        //fill(255);
+        //rect(xpos, ypos, 40, 40);
+        image(bird, xpos, ypos);
         vy +=gravity;
         ypos += vy;
         if(ypos > height - 40){
